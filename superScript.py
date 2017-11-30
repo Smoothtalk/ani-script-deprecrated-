0335d3d0d1cd11ea8e3e11008fac71fb1304a4f3
Avinash <avinash1010@gmail.com>
2017-11-30 11:50:55 -0700

    Json settings included

--- superScript.py
+++ superScript.py
@@ -1,17 +1,24 @@
 import sys
 import os
-import vars
+import json
+from collections import OrderedDict
 
-#Duckwad Stinx
+#python superScript.py "/home/downloads/Anime/[HorribleSubs] Just Because! - 08 [720p].mkv" "3A4038BF315EC6AE99FA073DFBF1702CEE412EF0" "[HorribleSubs] Just Because! - 08 [720p].mkv"
 
-os.chdir(vars.script_loc)
+def readJson():
+	json_data=open("vars.json").read()
+	data = json.loads(json_data, object_pairs_hook=OrderedDict)
+	return data #an OrderedDict
+
+settings = readJson()
+os.chdir(settings['System Settings']['script_location'])
 
 arg1 = sys.argv[1]
 arg2 = sys.argv[2]
 arg3 = sys.argv[3]
 
-#sys.argv = ['synckkk.py', arg1, arg3]
-#execfile('synckkk.py')
+sys.argv = ['synckkk.py', arg1, arg3]
+execfile('synckkk.py')
 
 sys.argv = ['sync.py', arg1, arg2, arg3]
 execfile('sync.py')
