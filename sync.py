fac44799ec4860e7c23004f1756cc915ae7be533
Avinash <avinash1010@gmail.com>
2017-11-30 12:19:43 -0700

    Fixed indentation on sync, and added fuzzy matching

--- sync.py
+++ sync.py
@@ -9,6 +9,7 @@ import time
 import subprocess
 import xml.etree.ElementTree as ET
 import multiprocessing
+from fuzzywuzzy import fuzz
 from multiprocessing import Process
 from collections import OrderedDict
 
@@ -18,39 +19,40 @@ def readJson():
 	return data #an OrderedDict
 
 def sync (x, hashed, settings, seriesName, episode, filename, userList, custom_title_Avi_len, custom_title_Kan_len):
-    print "Series: " + seriesName
-    print "Episode: " + episode
-    print x
+	print "Series: " + seriesName
+	print "Episode: " + episode
+	print x
 
-    index = 0
-    found = "false"
+	index = 0
+	found = "false"
 
-    command = "python retMal.py " + '\"' + userList[x] + '\"'
-    os.system(command)
-    file_input = open("flags", 'rb')
+	command = "python retMal.py " + '\"' + userList[x] + '\"'
+	os.system(command)
+	file_input = open("flags", 'rb')
 
-    database = userList[x] + ".xml"
+	database = userList[x] + ".xml"
 
-    with open(database, 'rt') as f:
-        tree = ET.parse(f)
+	with open(database, 'rt') as f:
+		tree = ET.parse(f)
 
-        for node in tree.findall('.//anime'):
-            raw_status = node.find('my_status').text
-            status = raw_status.strip()
-            if status == '1' or status == '6':
-                raw_title = node.find('series_title').text
-                raw_alt_title = node.find('series_synonyms').text #this is a list
-                raw_my_watched_episodes = node.find('my_watched_episodes').text
+		for node in tree.findall('.//anime'):
+			raw_status = node.find('my_status').text
+			status = raw_status.strip()
+			if status == '1' or status == '6':
+				raw_title = node.find('series_title').text
+				raw_alt_title = node.find('series_synonyms').text #this is a list
+				raw_my_watched_episodes = node.find('my_watched_episodes').text
 
-                title = raw_title.strip()
-                alt_title_unsplit = raw_alt_title.strip()
-                alt_title = alt_title_unsplit.split('; ')
-                my_watched_episodes = raw_my_watched_episodes.strip()
+				title = raw_title.strip()
+				alt_title_unsplit = raw_alt_title.strip()
+				alt_title = alt_title_unsplit.split('; ')
+				my_watched_episodes = raw_my_watched_episodes.strip()
 
-                for element in alt_title:
-                    if found == "false":
-                        if title == seriesName or element == seriesName:
-                            found = "true"
+				for element in alt_title:
+					if found == "false":
+						# if title == seriesName or element == seriesName:
+						if (fuzz.ratio(title, seriesName) > 70 or fuzz.ratio(element, seriesName)):
+							found = "true"
 
         if x == 0:
             listLen = custom_title_Avi_len
@@ -67,38 +69,35 @@ def sync (x, hashed, settings, seriesName, episode, filename, userList, custom_t
 
         if found == "true":
             if episode > int(my_watched_episodes):
+				if x == 0:
+					found = "true"
+					command = "rsync --progress -v -z -e 'ssh -p" + settings['Users']['Smoothtalk']['remote_port'] + "'" + " \"" + filePath + "\"" + ' ' + "\"" + settings['Users']['Smoothtalk']['remote_host'] + ":" + settings['Users']['Smoothtalk']['remote_download_dir'] + "\""
+					process = subprocess.check_call(command, shell=True)
+					command = "ssh -p" + settings['Users']['Smoothtalk']['remote_port'] + ' ' + settings['Users']['Smoothtalk']['remote_host'] +  " \"mv '" + settings['Users']['Smoothtalk']['remote_download_dir'] +  sys.argv[3] + "' '" + settings['Users']['Smoothtalk']['remote_download_dir'] + filename + "'\""
+					#process = subprocess.check_call(command, shell=True)
+				elif x == 1:
+					found = "true"
+					print "Kan"
+					command = "rsync --progress -v -z -e 'ssh -p" + settings['Users']['shinigamibob']['remote_port'] + "'" + " \"" + filePath + "\"" + ' ' + "\"" + settings['Users']['shinigamibob']['remote_host'] + ":" + settings['Users']['shinigamibob']['remote_download_dir'] + "\""
+					process =  subprocess.check_call(command, shell=True)
+					command = "ssh -p" + settings['Users']['shinigamibob']['remote_port'] + ' ' + settings['Users']['shinigamibob']['remote_host'] +  " \"mv '" + settings['Users']['shinigamibob']['remote_download_dir'] + sys.argv[3] + "' '" + settings['Users']['shinigamibob']['remote_download_dir'] + filename + "'\""
+					#process = subprocess.check_call(command, shell=True)
 
-		if x == 0:
-			found = "true"
-			command = "rsync --progress -v -z -e 'ssh -p" + settings['Users']['Smoothtalk']['remote_port'] + "'" + " \"" + filePath + "\"" + ' ' + "\"" + settings['Users']['Smoothtalk']['remote_host'] + ":" + settings['Users']['Smoothtalk']['remote_download_dir'] + "\""
-			process = subprocess.check_call(command, shell=True)
-			command = "ssh -p" + settings['Users']['Smoothtalk']['remote_port'] + ' ' + settings['Users']['Smoothtalk']['remote_host'] +  " \"mv '" + settings['Users']['Smoothtalk']['remote_download_dir'] +  sys.argv[3] + "' '" + settings['Users']['Smoothtalk']['remote_download_dir'] + filename + "'\""
-			#process = subprocess.check_call(command, shell=True)
-		elif x == 1:
-			found = "true"
-			print "Kan"
-			command = "rsync --progress -v -z -e 'ssh -p" + settings['Users']['shinigamibob']['remote_port'] + "'" + " \"" + filePath + "\"" + ' ' + "\"" + settings['Users']['shinigamibob']['remote_host'] + ":" + settings['Users']['shinigamibob']['remote_download_dir'] + "\""
-			process =  subprocess.check_call(command, shell=True)
-			command = "ssh -p" + settings['Users']['shinigamibob']['remote_port'] + ' ' + settings['Users']['shinigamibob']['remote_host'] +  " \"mv '" + settings['Users']['shinigamibob']['remote_download_dir'] + sys.argv[3] + "' '" + settings['Users']['shinigamibob']['remote_download_dir'] + filename + "'\""
-			# subprocess.call(command, shell=True)
-
-		if hashed == 0:
-			os.chdir(settings['System Settings']['script_location'])
-            completed = open("completed.txt", "a")
-            completed.write(hash)
-            completed.write('\n')
-            completed.close()
-            hashed = 1
-        elif x == 1:
-            if hashed == 0:
-                os.chdir(settings['System Settings']['script_location'])
-                completed = open("completed.txt", "a")
-                completed.write(hash)
-                completed.write('\n')
-                completed.close()
-
-	os.system(command)
-    return
+				if hashed == 0:
+					os.chdir(settings['System Settings']['script_location'])
+					completed = open("completed.txt", "a")
+					completed.write(hash)
+					completed.write('\n')
+					completed.close()
+					hashed = 1
+				elif x == 1:
+					if hashed == 0:
+						os.chdir(settings['System Settings']['script_location'])
+						completed = open("completed.txt", "a")
+						completed.write(hash)
+						completed.write('\n')
+						completed.close()
+	return
 
 if __name__=='__main__':
     settings = readJson()
