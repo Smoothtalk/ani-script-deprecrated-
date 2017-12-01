dbeebed129ca056965048dc7bb627e03b80eb7cb
Avinash <avinash1010@gmail.com>
2017-11-30 17:44:02 -0700

    Implemented feature to announce to discord client

--- discordAnnounce.py
+++ discordAnnounce.py
@@ -3,3 +3,40 @@ import os
 import random
 import datetime
 import discord
+import json
+import sys
+from collections import OrderedDict
+
+token = "MzU3NjA2NDQ0ODMwMjk0MDE3.DKHIMg.AWDhiu2xyLSHuRg-l0SSuiYJD5s"
+
+discordClient = discord.Client()
+
+def readJson():
+	json_data=open("vars.json").read()
+	data = json.loads(json_data, object_pairs_hook=OrderedDict)
+	return data #an OrderedDict
+
+async def announce():
+    await discordClient.wait_until_ready()
+    settings = readJson()
+
+    if(sys.argv[2] == "0"):
+        messageRecipient = await discordClient.get_user_info(settings['Users']['Smoothtalk']['discord_ID'])
+        await discordClient.send_message(messageRecipient, str(sys.argv[1]) + " successfully synced")
+    elif(sys.argv[2] == "1"):
+        messageRecipient = await discordClient.get_user_info(settings['Users']['shinigamibob']['discord_ID'])
+        await discordClient.send_message(messageRecipient, str(sys.argv[1]) + " successfully synced")
+    else:
+        pass
+
+    await discordClient.logout()
+
+@discordClient.event
+async def on_ready():
+    print('Logged in as')
+    print(discordClient.user.name)
+    print(discordClient.user.id)
+    await announce()
+    print('-------')
+
+discordClient.run(token)
