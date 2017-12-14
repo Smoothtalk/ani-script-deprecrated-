e3fe3587aa2a0d3c69393e042dbf7a5f404525f5
Avinash <avinash1010@gmail.com>
2017-12-13 17:53:48 -0700

    Major Refactoring

--- sync.py
+++ sync.py
@@ -13,14 +13,51 @@ from fuzzywuzzy import fuzz
 from multiprocessing import Process
 from collections import OrderedDict
 
+class Series:
+	seriesName = ""
+	episode = ""
+	fileName = ""
+	filePath = ""
+
+	def getSeriesName(self):
+		return self.seriesName
+
+	def getSeriesEpisode(self):
+		return self.episode
+
+	def getSeriesTitle(self, fileName):
+		tempName = fileName.replace("_", " ")
+		firstHyphen = tempName.rfind(' - ')
+		firstCBrac = tempName.index(']', 0)
+		seriesName = tempName[firstCBrac+2:firstHyphen]
+		episode = tempName[firstHyphen+3:]
+		episode = episode[:episode.index(' ',0)]
+		filename = seriesName + ' - ' + episode + '.mkv'
+
+		seriesName = seriesName.strip()
+
+		self.seriesName = seriesName
+		self.episode = episode
+		self.fileName = filename
+
+	def getFileName(self):
+		return self.fileName
+
+	def setFilePath(self, filePath):
+		self.filePath = filePath
+
+	def getFilePath(self):
+		return self.filePath
+
 def readJson():
 	json_data=open("vars.json").read()
 	data = json.loads(json_data, object_pairs_hook=OrderedDict)
 	return data #an OrderedDict
 
-def sync (x, hashed, settings, seriesName, episode, filename, userList, custom_title_Avi_len, custom_title_Kan_len):
-	print "Series: " + seriesName
-	print "Episode: " + episode
+#def sync (x, hashed, settings, seriesName, episode, filename, userList, custom_title_Avi_len, custom_title_Kan_len):
+def sync (x, hashed, settings, serialToSync, userList, custom_title_Avi_len, custom_title_Kan_len):
+	print "Series: " + 	serialToSync.getSeriesName()
+	print "Episode: " + serialToSync.getSeriesEpisode()
 	print x
 
 	index = 0
@@ -51,7 +88,7 @@ def sync (x, hashed, settings, seriesName, episode, filename, userList, custom_t
 				for element in alt_title:
 					if found == "false":
 						# if title == seriesName or element == seriesName:
-						if (fuzz.ratio(title, seriesName) > 70 or fuzz.ratio(element, seriesName) > 70):
+						if (fuzz.ratio(title, serialToSync.getSeriesName()) > 70 or fuzz.ratio(element, serialToSync.getSeriesName()) > 70):
 							found = "true"
 
         if x == 0:
@@ -63,17 +100,17 @@ def sync (x, hashed, settings, seriesName, episode, filename, userList, custom_t
 
         while index < listLen and found == "false":
             CustomTitle = customTitleList[index].strip()
-            if CustomTitle == seriesName:
+            if CustomTitle == serialToSync.getSeriesName():
                 found = "true"
             index+=1;
 
-        if found == "true" or (x == 0 and seriesName == 'Ame-con!!'):
-            if episode > int(my_watched_episodes):
+        if found == "true" or (x == 0 and serialToSync.getSeriesName() == 'Ame-con!!'):
+            if serialToSync.getSeriesEpisode() > int(my_watched_episodes):
 				if x == 0:
 					found = "true"
-					command = "rsync --progress -v -z -e 'ssh -p" + settings['Users']['Smoothtalk']['remote_port'] + "'" + " \"" + filePath + "\"" + ' ' + "\"" + settings['Users']['Smoothtalk']['remote_host'] + ":" + settings['Users']['Smoothtalk']['remote_download_dir'] + "\""
+					command = "rsync --progress -v -z -e 'ssh -p" + settings['Users']['Smoothtalk']['remote_port'] + "'" + " \"" + serialToSync.getFilePath() + "\"" + ' ' + "\"" + settings['Users']['Smoothtalk']['remote_host'] + ":" + settings['Users']['Smoothtalk']['remote_download_dir'] + "\""
 					process = subprocess.check_call(command, shell=True)
-					command = "ssh -p" + settings['Users']['Smoothtalk']['remote_port'] + ' ' + settings['Users']['Smoothtalk']['remote_host'] +  " \"mv '" + settings['Users']['Smoothtalk']['remote_download_dir'] +  sys.argv[3] + "' '" + settings['Users']['Smoothtalk']['remote_download_dir'] + filename + "'\""
+					command = "ssh -p" + settings['Users']['Smoothtalk']['remote_port'] + ' ' + settings['Users']['Smoothtalk']['remote_host'] +  " \"mv '" + settings['Users']['Smoothtalk']['remote_download_dir'] +  sys.argv[3] + "' '" + settings['Users']['Smoothtalk']['remote_download_dir'] + serialToSync.getFileName() + "'\""
 					process = subprocess.check_call(command, shell=True)
 					command = "python3.5 discordAnnounce.py \'" + sys.argv[3] + '\' ' + "0"
 					process = subprocess.call(command, shell=True)
@@ -87,9 +124,9 @@ def sync (x, hashed, settings, seriesName, episode, filename, userList, custom_t
 				elif x == 1:
 					found = "true"
 					print "Kan"
-					command = "rsync --progress -v -z -e 'ssh -p" + settings['Users']['shinigamibob']['remote_port'] + "'" + " \"" + filePath + "\"" + ' ' + "\"" + settings['Users']['shinigamibob']['remote_host'] + ":" + settings['Users']['shinigamibob']['remote_download_dir'] + "\""
+					command = "rsync --progress -v -z -e 'ssh -p" + settings['Users']['shinigamibob']['remote_port'] + "'" + " \"" + serialToSync.getFilePath() + "\"" + ' ' + "\"" + settings['Users']['shinigamibob']['remote_host'] + ":" + settings['Users']['shinigamibob']['remote_download_dir'] + "\""
 					process =  subprocess.check_call(command, shell=True)
-					command = "ssh -p" + settings['Users']['shinigamibob']['remote_port'] + ' ' + settings['Users']['shinigamibob']['remote_host'] +  " \"mv '" + settings['Users']['shinigamibob']['remote_download_dir'] + sys.argv[3] + "' '" + settings['Users']['shinigamibob']['remote_download_dir'] + filename + "'\""
+					command = "ssh -p" + settings['Users']['shinigamibob']['remote_port'] + ' ' + settings['Users']['shinigamibob']['remote_host'] +  " \"mv '" + settings['Users']['shinigamibob']['remote_download_dir'] + sys.argv[3] + "' '" + settings['Users']['shinigamibob']['remote_download_dir'] + serialToSync.getFileName() + "'\""
 					process = subprocess.check_call(command, shell=True)
 					command = "python3.5 discordAnnounce.py \'" + sys.argv[3] + '\' ' + "1"
 					process = subprocess.call(command, shell=True)
@@ -105,39 +142,33 @@ def sync (x, hashed, settings, seriesName, episode, filename, userList, custom_t
 
 if __name__=='__main__':
 	try:
-	    settings = readJson()
-	    #change sys.argv[1] to renamed
-	    hash = sys.argv[2]
-	    hashed = 0
+		settings = readJson()
+		#change sys.argv[1] to renamed
+		hash = sys.argv[2]
+		hashed = 0
 
-	    #substring the torrent name. If the script throws an exception here later
-	    #on, switch index to find
-	    tempName = sys.argv[3].replace("_", " ")
-	    firstHyphen = tempName.rfind(' - ')
-	    firstCBrac = tempName.index(']', 0)
-	    seriesName = tempName[firstCBrac+2:firstHyphen]
-	    episode = tempName[firstHyphen+3:]
-	    episode = episode[:episode.index(' ',0)]
-	    filename = seriesName + ' - ' + episode + '.mkv'
+		#substring the torrent name. If the script throws an exception here later
+		#on, switch index to find
+		serialToSync = Series()
+		serialToSync.getSeriesTitle(sys.argv[3])
+		serialToSync.setFilePath(settings['System Settings']['host_download_dir'] + sys.argv[3])
 
-	    seriesName = seriesName.strip()
-	    custom_title_Avi_len = len(settings['Users']['Smoothtalk']['custom_titles'])
-	    custom_title_Kan_len = len(settings['Users']['shinigamibob']['custom_titles'])
+		custom_title_Avi_len = len(settings['Users']['Smoothtalk']['custom_titles'])
+		custom_title_Kan_len = len(settings['Users']['shinigamibob']['custom_titles'])
 
-	    #for automation tools because PATH is hard
-	    os.chdir(settings['System Settings']['script_location'])
-	    filePath = settings['System Settings']['host_download_dir'] + sys.argv[3]
-	    path = sys.argv[1]
+		#for automation tools because PATH is hard
+		os.chdir(settings['System Settings']['script_location'])
+		path = sys.argv[1]
 
-	    userList = settings['Users'].keys()
+		userList = settings['Users'].keys()
 
-	    if "downloads/Anime" not in path:
-	        sys.exit(1)
+		if "downloads/Anime" not in path:
+			sys.exit(1)
 
-	    jobs = []
-	    for x in range(len(settings['Users'])):
-	        p = multiprocessing.Process(target=sync, args=(x, hashed, settings, seriesName, episode, filename, userList, custom_title_Avi_len, custom_title_Kan_len))
-	        jobs.append(p)
-	        p.start()
-	except:
-		print "failed"
+		jobs = []
+		for x in range(len(settings['Users'])):
+			p = multiprocessing.Process(target=sync, args=(x, hashed, settings, serialToSync, userList, custom_title_Avi_len, custom_title_Kan_len))
+			jobs.append(p)
+			p.start()
+	except Exception as e:
+		print e
