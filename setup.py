af3381fac76cb2aaf0cf93177393bd2540aef413
Avinash <avinash1010@gmail.com>
2018-01-19 16:01:29 -0700

    Initial setup of checking commands running on system.

--- setup.py
+++ setup.py
@@ -1,19 +1,56 @@
-import vars
 import os
-user = raw_input('Are you Avi or Kanchana: ')
-user.lower()
+import subprocess
 
-if(user == "avi"):
-	command = "ssh -p" + vars.userport1 + ' ' + vars.a_host + " mkdir -p .ssh"
-	os.system(command)
-	print "one more time (we're going to celebrate)"
-	command = "cat ~/.ssh/id_rsa.pub | ssh -p" + vars.userport1 + ' ' + vars.a_host + " \'cat >> ~/.ssh/authorized_keys\'"
-	os.system(command)
-elif(user == "kanchana"):
-        command = "ssh -p" + vars.userport2 + ' ' + vars.k_host + " mkdir -p .ssh"
-        os.system(command)
-	print "one more time (we're going to celebrate)"
-        command = "cat ~/.ssh/id_rsa.pub | ssh -p" + vars.userport2 + ' ' + vars.k_host + " \'cat >> ~/.ssh/authorized_keys\'"
-        os.system(command)
-else:
-	print "you are not valid, bitch"
+try:
+    subprocess.call(["pip", "install", "bs4"])
+except OSError as e:
+	print (type(e))
+	if e.errno == os.errno.ENOENT:
+		print ("FNF")
+	else:
+		raise
+
+# try:
+#     subprocess.call([""])
+# except OSError as e:
+#     if e.errno == os.errno.ENOENT:
+#         # handle file not found error.
+#     else:
+#         # Something else went wrong while trying to run `wget`
+#         raise
+#
+# try:
+#     subprocess.call([""])
+# except OSError as e:
+#     if e.errno == os.errno.ENOENT:
+#         # handle file not found error.
+#     else:
+#         # Something else went wrong while trying to run `wget`
+#         raise
+#
+# try:
+#     subprocess.call([""])
+# except OSError as e:
+#     if e.errno == os.errno.ENOENT:
+#         # handle file not found error.
+#     else:
+#         # Something else went wrong while trying to run `wget`
+#         raise
+#
+# try:
+#     subprocess.call([""])
+# except OSError as e:
+#     if e.errno == os.errno.ENOENT:
+#         # handle file not found error.
+#     else:
+#         # Something else went wrong while trying to run `wget`
+#         raise
+#
+# try:
+#     subprocess.call([""])
+# except OSError as e:
+#     if e.errno == os.errno.ENOENT:
+#         # handle file not found error.
+#     else:
+#         # Something else went wrong while trying to run `wget`
+#         raise
