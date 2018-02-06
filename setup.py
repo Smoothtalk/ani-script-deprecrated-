e2c09441ee7b271f132af38b58988e5c8de18367
Avinash <avinash1010@gmail.com>
2018-02-06 15:25:37 -0700

    Initial installing missing deps file

--- setup.py
+++ setup.py
@@ -1,56 +1,65 @@
 import os
 import subprocess
 
-try:
-    subprocess.call(["pip", "install", "bs4"])
-except OSError as e:
-	print (type(e))
-	if e.errno == os.errno.ENOENT:
-		print ("FNF")
-	else:
-		raise
+dependencies = ['bs4', 'fuzzywuzzy']
 
-# try:
-#     subprocess.call([""])
-# except OSError as e:
-#     if e.errno == os.errno.ENOENT:
-#         # handle file not found error.
-#     else:
-#         # Something else went wrong while trying to run `wget`
-#         raise
-#
-# try:
-#     subprocess.call([""])
-# except OSError as e:
-#     if e.errno == os.errno.ENOENT:
-#         # handle file not found error.
-#     else:
-#         # Something else went wrong while trying to run `wget`
-#         raise
-#
-# try:
-#     subprocess.call([""])
-# except OSError as e:
-#     if e.errno == os.errno.ENOENT:
-#         # handle file not found error.
-#     else:
-#         # Something else went wrong while trying to run `wget`
-#         raise
-#
-# try:
-#     subprocess.call([""])
-# except OSError as e:
-#     if e.errno == os.errno.ENOENT:
-#         # handle file not found error.
-#     else:
-#         # Something else went wrong while trying to run `wget`
-#         raise
-#
-# try:
-#     subprocess.call([""])
-# except OSError as e:
-#     if e.errno == os.errno.ENOENT:
-#         # handle file not found error.
-#     else:
-#         # Something else went wrong while trying to run `wget`
-#         raise
+def getList():
+	try:
+		packages = []
+		process = subprocess.Popen(["sudo", "pip", "list", "--format=legacy"], stdout=subprocess.PIPE)
+		while True:
+			output = process.stdout.readline()
+			if output != b'':
+				package_Name = output[0:str(output).find(' ', 0)-2]
+				package_Version = output[str(output).rfind(' '):len(str(output))-6].strip()
+				#print ("name: " + str(package_Name) + " version: " + str(package_Version))
+				installed_package = {'Name': str(package_Name).strip(), 'Version': str(package_Version).strip()}
+				packages.append(installed_package)
+			else:
+				break
+		return packages
+	except OSError as e:
+		print (type(e))
+		if e.errno == os.errno.ENOENT:
+			print ("File Not Found?")
+		else:
+			raise
+
+def checkList(packages):
+	allPackageNames = []
+	remainingDependicies = []
+	index = 0
+
+	for element in dependencies:
+		remainingDependicies.append(element)
+
+	for package in packages:
+		allPackageNames.append(packages[index]['Name'])
+		index+=1
+
+	for term in dependencies:
+		byteTerm = 'b\'' + term + '\'' #add binary typing ish
+		if byteTerm in allPackageNames:
+			remainingDependicies.remove(term)
+
+	return remainingDependicies
+
+def installMissingDependicies(dependencies):
+	failedToFind = b'No matching distribution found for'
+	for package in dependencies:
+		process = subprocess.Popen(["sudo", "pip", "install", package], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		while True:
+			output = process.stdout.readline()
+			error = process.stderr.readline()
+			if(failedToFind in error):
+				break
+			if output == '' and process.poll() is not None:
+				break
+		rc = process.poll()
+
+packages = getList()
+theDependiciesLeft = checkList(packages)
+if(len(theDependiciesLeft) == 0):
+	print ("All Deps installed")
+else:
+	installMissingDependicies(theDependiciesLeft)
