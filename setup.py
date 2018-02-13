54b6ca0800ca8fce3e06c600c41c644fd26f3ad9
Avinash <avinash1010@gmail.com>
2018-02-13 13:03:49 -0700

    crontab hardcoded adding in lines

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 import os
+import sys
 import subprocess
 
 dependencies = ['bs4', 'fuzzywuzzy', 'trakt', 'simplejson', 'feedparser']
 dependencies3 = ['discord.py', 'multidict', 'websockets']
 noRtorrent = "The program \'rtorrent\' is currently not installed. You can install it by typing:\napt install rtorrent"
+rtorrentAuto = '#system.method.set_key=event.download.finished,ascript,\"execute=python,/home/seedbox/ani-script/superScript.py,$d.get_base_path=,$d.get_hash=,$d.get_name=\"'
+cronTabData = "0,30 * * * * cd /home/seedbox/ani-script && python pull2.py > /tmp/pull2.log\n5,35 * * * * cd /home/seedbox/ani-script && python pull.py > /tmp/pull.log\n0,0 * * * 0 cd /home/seedbox/ani-script && python delete.py > /tmp/delete.log\n5,0 * * * 0 cd /home/seedbox/downloads/Anime && find -size 0 -delete\n"
 
 def checkRutorrent():
 	try:
@@ -12,7 +15,20 @@ def checkRutorrent():
 			output = process.stdout.readline()
 			error = process.stderr.readline()
 			print ("Rtorrent is installed")
-			#add to rtorrent.rc the line
+			os.chdir(os.path.expanduser('~'))
+			lineInFile = False
+			with open(".rtorrent.rc", "r") as ins:
+				for line in ins:
+					if line.strip() == rtorrentAuto.strip():
+						lineInFile = True
+			ins.close()
+
+			if(lineInFile == False):
+				ins = open(".rtorrent.rc", "a")
+				ins.write(rtorrentAuto)
+				ins.write('\n')
+				ins.close()
+
 			return
 	except OSError as e:
 		if e.errno == os.errno.ENOENT:
@@ -21,7 +37,62 @@ def checkRutorrent():
 			raise
 
 def cronTabAdding():
-	pass
+	homedir = os.environ['HOME']
+	try:
+		process = subprocess.Popen(["crontab", "-l"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		while True:
+			output = process.stdout.readline()
+			error = process.stderr.readline()
+			noCronMessage = "b\'no crontab for " + str(homedir[6:] + '\'') #darn byte notation
+			if str(error.strip()) == noCronMessage:
+				tempCron = open("cron.tmp", "w+")
+				tempCron.write(cronTabData)
+				tempCron.close()
+				process = subprocess.Popen(["crontab", "-u", homedir[6:], "cron.tmp"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+				output = process.stdout.readline()
+				error = process.stderr.readline()
+				os.remove("cron.tmp")
+			else:
+				cronList = cronTabData.split('\n')
+				del cronList[4]
+				process = subprocess.Popen(["crontab", "-l"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+				tempCron = open("cron.tmp", "w+")
+				#write output to file and then append the missing commands from remaining cronlist
+				while True:
+					output = process.stdout.readline()
+					outputClean = str(output, 'utf-8')
+					tempCron.write(outputClean)
+					if outputClean == '' or outputClean == '\n':
+						tempCron.close()
+						break
+					if output:
+						for lines in cronList:
+							if (str(output) == 'b\'' + lines + '\\n\''):
+								print ("Line Found")
+								cronList.remove(lines)
+
+				if(len(cronList) == 0):
+					pass
+					print("all commands Found")
+					os.remove("cron.tmp")
+				else:
+					#one or more commands are missing
+					print ("Dank")
+					tempCron = open("cron.tmp", "a+")
+					for lines in cronList:
+						tempCron.write(lines + '\n')
+					tempCron.close()
+					process = subprocess.Popen(["crontab", "-u", homedir[6:], "cron.tmp"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+					output = process.stdout.readline()
+					error = process.stderr.readline()
+					os.remove("cron.tmp")
+			break
+	except OSError as e:
+		print (type(e))
+		if e.errno == os.errno.ENOENT:
+			print ("File Not Found?")
+		else:
+			raise
 
 def checkForRTcontrol():
 	pass
@@ -142,16 +213,17 @@ def installMissing3Dependicies(dependencies):
 				pass
 		rc = process.poll()
 
-checkRutorrent()
-packages = getPipList()
-packages3 = getPip3List()
-theDependiciesLeft = checkList(packages)
-theDependicies3Left = check3List(packages3)
-if(len(theDependicies3Left) == 0):
-	print ("All Deps of Pip3 installed")
-else:
-	installMissing3Dependicies(theDependicies3Left)
-if(len(theDependiciesLeft) == 0):
-	print ("All Deps of Pip installed")
-else:
-	installMissingDependicies(theDependiciesLeft)
+cronTabAdding()
+#checkRutorrent()
+# packages = getPipList()
+# packages3 = getPip3List()
+# theDependiciesLeft = checkList(packages)
+# theDependicies3Left = check3List(packages3)
+# if(len(theDependicies3Left) == 0):
+# 	print ("All Deps of Pip3 installed")
+# else:
+# 	installMissing3Dependicies(theDependicies3Left)
+# if(len(theDependiciesLeft) == 0):
+# 	print ("All Deps of Pip installed")
+# else:
+# 	installMissingDependicies(theDependiciesLeft)
