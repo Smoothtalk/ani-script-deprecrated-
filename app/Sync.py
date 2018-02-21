ddaff8675a059ad20d3fb3b06a1e38297fa682d6
Avinash <avinash1010@gmail.com>
2018-02-21 12:31:07 -0700

    Test changes

--- app/Sync.py
+++ app/Sync.py
@@ -5,7 +5,7 @@ import json
 from collections import OrderedDict
 
 #python superScript.py "/home/downloads/Anime/[HorribleSubs] Just Because! - 08 [720p].mkv" "3A4038BF315EC6AE99FA073DFBF1702CEE412EF0" "[HorribleSubs] Just Because! - 08 [720p].mkv"
-#python synckkk.py "/home/seedbox/downloads/Anime/Tosh.0.S09E01.720p.HDTV.x264-MiNDTHEGAP[rarbg]" "Tosh.0.S09E01.720p.HDTV.x264-MiNDTHEGAP[rarbg]"
+#python synckkk.py "/home/downloads/Anime/Mr.Robot.S03E10.720p.HDTV.x264-KILLERS[rarbg]" "Mr.Robot.S03E10.720p.HDTV.x264-KILLERS[rarbg]"
 
 def readJson():
 	json_data=open("vars.json").read()
@@ -19,15 +19,15 @@ arg1 = sys.argv[1]
 arg2 = sys.argv[2]
 arg3 = sys.argv[3]
 
-# try:
-#     command = 'python synckkk.py \'' + arg1 + '\' \'' + arg3 + '\''
-#     process = subprocess.call(command, shell=True)
-# except Exception as e:
-#     print "Failed to sync white show"
-
 try:
-	command = 'python3.5 Core/AnimeSync.py \'' + arg1 + '\' \'' + arg2 + '\' \'' + arg3 + '\''
-	process = subprocess.call(command, shell=True)
+    command = 'python3.5 Core/ShowSync.py \'' + arg1 + '\' \'' + arg3 + '\''
+    process = subprocess.call(command, shell=True)
 except Exception as e:
-	print (e)
-	print ("Failed to sync anime")
+    print ("Failed to sync white show")
+
+# try:
+# 	command = 'python3.5 Core/AnimeSync.py \'' + arg1 + '\' \'' + arg2 + '\' \'' + arg3 + '\''
+# 	process = subprocess.call(command, shell=True)
+# except Exception as e:
+# 	print (e)
+# 	print ("Failed to sync anime")
