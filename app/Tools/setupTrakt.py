0965ae97aac35a799f47cd9ee5a676da7edd2f47
Avinash <avinash1010@gmail.com>
2018-02-26 08:09:20 -0700

    File Structure update

--- app/Tools/setupTrakt.py
+++ app/Tools/setupTrakt.py
@@ -4,7 +4,7 @@ import json
 from collections import OrderedDict
 
 def readJson():
-	json_data=open("vars.json").read()
+	json_data=open("../vars.json").read()
 	data = json.loads(json_data, object_pairs_hook=OrderedDict)
 	return data #an OrderedDict
 
@@ -16,4 +16,3 @@ my_client_id = settings['Trakt Settings']['client_id']
 my_client_secret = settings['Trakt Settings']['secret_id']
 
 init(settings['Users']["Smoothtalk"]['traktUserName'], client_id=my_client_id, client_secret=my_client_secret, store=True)
-#init("Smoothtalk", client_id='31dc283b13ac16dc6e6f31fccfa0bbdeafbdf2db089c555b7b90ac581fd02daa', client_secret='55e1a69191ed9fd1339f26c41d245be50bec0ce22362645f1180535c14a15f82', store=True)
