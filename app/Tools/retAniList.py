6f02d8c5c30588907adf499d94c2b0bc77700454
Smoothtalk <avinash1010@gmail.com>
2018-07-05 09:38:43 -0600

    added extra fields, cleaned up order

--- app/Tools/retAniList.py
+++ app/Tools/retAniList.py
@@ -7,7 +7,7 @@ import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
 
 username = sys.argv[1]
-database = "Data/" + username + ".xml"
+database = "Data/" + username + ".json"
 
 url = 'https://graphql.anilist.co'
 
@@ -20,14 +20,17 @@ animeListQuery = '''
 query getAnimeList ($userName : String, $type : MediaType) {
   MediaListCollection (userName : $userName, type : $type) {
     lists{
+        status
         entries {
+            progress
+            mediaId
             media {
-                title {romaji, english}
                 status
+                synonyms
+                title {romaji, english}
                 startDate {year,month,day}
                 endDate {year,month,day}
                 }
-            progress
             }
         }
     }
@@ -37,6 +40,7 @@ query getAnimeList ($userName : String, $type : MediaType) {
 response = requests.post(url, json={'query': animeListQuery, 'variables': listVariables})
 
 prettySoup = json.dumps(response.json()['data']['MediaListCollection']['lists'], indent=4)
+
 output = codecs.open(database, 'wb', 'utf-8')
 output.write(prettySoup)
 output.close()
